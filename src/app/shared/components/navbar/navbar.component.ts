82d26903e693f63d69ea0821bf2b4a693b12ff3f
HendAdel1 <1hend.adel1@gmail.com>
2025-10-06 15:58:36 +0300

    navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { FlowbiteService } from '../../../core/services/flowbite.service';
+import { initFlowbite } from 'flowbite';
+import { RouterLink } from "@angular/router";
+import { RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  pages: {title: string, path: string}[] = [
+    {title: 'Home', path: 'home'},
+    {title: 'Products', path: 'products'},
+    {title: 'Categories', path: 'categories'},
+    {title: 'Brands', path: 'brands'},
+    {title: 'Cart', path: 'cart'},
+  ]
+
+   authPages: {title: string, path: string}[] = [
+    {title: 'Login', path: 'login'},
+    {title: 'Register', path: 'register'},
+   ]
+
+ constructor(private flowbiteService: FlowbiteService) {}
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
+
+  logOut(){
+
+  }
 }
