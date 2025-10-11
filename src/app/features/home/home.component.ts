e5fb103f83ebf93eca033d37c91040e462af3380
HendAdel1 <1hend.adel1@gmail.com>
2025-10-11 19:22:45 +0300

    thumbnails of product details

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,41 +1,41 @@
 import { ProductService } from './../../core/services/product.service';
 import { Component, OnInit } from '@angular/core';
-import { ProductCardComponent } from "../../shared/components/product-card/product-card.component";
+import { ProductCardComponent } from '../../shared/components/product-card/product-card.component';
 import { Product } from '../../core/models/data.interface';
 import { CommonModule } from '@angular/common';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
   imports: [CommonModule, ProductCardComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit{
-
+export class HomeComponent implements OnInit {
   isLoading = false;
   skeletons = Array(10);
   products: Product[] = [];
 
-  constructor( private productService: ProductService){}
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-      this.getAllProducts();
+    this.getAllProducts();
   }
 
- getAllProducts() {
-  this.isLoading = true;
-  // simulate slow API
-  setTimeout(() => {
-    this.productService.getAllProducts({}).subscribe({
-      next: (response) => {
-        this.products = response.data;
-        this.isLoading = false;
-      },
-      error: () => {
-        this.isLoading = false;
-      }
-    });
-  }, 1500);
-}
+  getAllProducts() {
+    this.isLoading = true;
+    // simulate slow API
+    setTimeout(() => {
+      this.productService.getAllProducts({}).subscribe({
+        next: (response) => {
+          this.products = response.data;
+          this.isLoading = false;
+        },
+        error: () => {
+          this.isLoading = false;
+        },
+      });
+    }, 1500);
+  }
 
 }
--- src/app/features/product-details/product-details.component.ts
+++ src/app/features/product-details/product-details.component.ts
@@ -7,44 +7,47 @@ import { Product } from '../../core/models/data.interface';
   selector: 'app-product-details',
   imports: [],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  styleUrl: './product-details.component.css',
+  
 })
-export class ProductDetailsComponent implements OnInit{
+export class ProductDetailsComponent implements OnInit {
 
   product: Product | null = null;
   isLoading = false;
+  selectedImage: string | null = null;
 
-  
-  constructor (
+  constructor(
     private productService: ProductService,
     private route: ActivatedRoute,
-    
-  ){}
+
+  ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       const id = params.get("id")
-      if(id){
+      if (id) {
         this.getSpecificProduct(id);
       }
     })
   }
-
-
-  getSpecificProduct(id: string){
+  
+  getSpecificProduct(id: string) {
     this.isLoading = true;
-
+    
     this.productService.getSpecificProduct(id).subscribe({
-      next:(response) =>{
-        if(response.data){
+      next: (response) => {
+        if (response.data) {
           this.product = response.data;
-            this.isLoading = false;
+          this.isLoading = false;
         }
       },
       error: (error) => {
-            this.isLoading = false;
+        this.isLoading = false;
       }
     })
   }
-
+  
+  changeMainImage(image: string) {
+    this.selectedImage = image;
+  }
 }
